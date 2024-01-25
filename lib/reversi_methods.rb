c76b7023607cb6d8182b6e6a69ffeb00cf662888
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-25 23:30:43 +0900

    rowとcolの計算が逆になっていたため修正しました。

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -67,7 +67,7 @@ module ReversiMethods
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
-      board[target_pos.row][target_pos.col] = attack_stone_color
+      board[target_pos.col][target_pos.row] = attack_stone_color
       true
     else
       false
